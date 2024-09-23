b32b0a634008388ddc8548d5ab308e5997c9ceaf
Jonathan Benz <98258254+JonathanBenz@users.noreply.github.com>
2024-09-23 01:42:54 -0400

    Added Sprint Button

--- Sample2DProject/Assets/Scripts/PlayerMovement.cs
+++ Sample2DProject/Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,9 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
     float playerSpeed = 20f;
+    [SerializeField] [Range(1, 2)]
+    float sprintMultiplier = 1.5f;
+    bool isSprinting;
 
     Vector2 movementInput = Vector2.zero;
     Rigidbody rb;
@@ -20,12 +23,21 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 movement = new Vector3(movementInput.x, 0f, movementInput.y);
+        Vector3 movement;
+        if (isSprinting)
+        {
+            movement = new Vector3(movementInput.x, 0f, movementInput.y) * sprintMultiplier;
+        }
+        else movement = new Vector3(movementInput.x, 0f, movementInput.y);
         rb.velocity += movement * playerSpeed * Time.deltaTime;
     }
     public void OnMove(InputAction.CallbackContext context)
     {
         movementInput = context.ReadValue<Vector2>();
+    }
+    public void OnSprint(InputAction.CallbackContext context)
+    {
+        isSprinting = context.performed;
     }
     /*private bool CheckForOtherPlayer()
     {
