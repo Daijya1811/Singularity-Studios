d55e6763aa64047c81e311065d4cd14ee5f47a2d
William Tallarico <wct9667@rit.edu>
2024-09-26 15:13:21 -0400

    navmesh

---
+++ Sample2DProject/Assets/Scripts/ClickPlayerMovement.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class ClickPlayerMovement : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent agent;
+    private MouseInput mouseInput;
+
+    private Camera cam;
+    
+    private static readonly int GroundLayer = 1 << 6;
+
+    private void Awake()
+    {
+        mouseInput = new MouseInput();
+    }
+
+    private void OnEnable()
+    {
+        mouseInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        mouseInput.Disable();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        cam = Camera.main;
+        mouseInput.Mouse.MouseClick.performed += _ => MouseClick();
+    }
+
+    void MouseClick()
+    {
+        Vector3 mousePosition = mouseInput.Mouse.MousePosition.ReadValue<Vector2>();
+        Ray ray = cam.ScreenPointToRay(mousePosition);
+        RaycastHit hit;
+        Debug.DrawRay(ray.origin, ray.direction * 1000, Color.magenta, 1000);
+
+        if (Physics.Raycast((ray), out hit, 1000f, GroundLayer))
+        {
+            Debug.Log("hit");
+            agent.destination = hit.point;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+}
