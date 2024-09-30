0d464b562247dfde95d80b889ce879da034cff5a
William Tallarico <wct9667@rit.edu>
2024-09-30 13:01:29 -0400

    updated players

--- Sample2DProject/Assets/Scripts/ClickPlayerMovement.cs
+++ Sample2DProject/Assets/Scripts/ClickPlayerMovement.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(NavMeshAgent))]
 public class ClickPlayerMovement : MonoBehaviour
@@ -10,10 +11,15 @@ public class ClickPlayerMovement : MonoBehaviour
     [SerializeField] private NavMeshAgent agent;
     private Input mouseInput;
 
+    [SerializeField] private float speed = 3.5f;
+    [SerializeField] private float sprintMultiplier = 2f;
+
     private Camera cam;
     
     private static readonly int GroundLayer = 1 << 6;
 
+
+    private bool isSprinting;
     private void Awake()
     {
         mouseInput = new Input();
@@ -50,9 +56,16 @@ public class ClickPlayerMovement : MonoBehaviour
             agent.destination = hit.point;
         }
     }
+
+    public void OnSprint(InputAction.CallbackContext context)
+    {
+        isSprinting = context.performed;
+    }
+    
     // Update is called once per frame
     void Update()
     {
-      
+        if (isSprinting) agent.speed  *= sprintMultiplier;
+        else agent.speed = speed;
     }
 }
