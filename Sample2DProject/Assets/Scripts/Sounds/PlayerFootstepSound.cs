4291704c54824cc99f2cb128fd9b23ebe036e772
William Tallarico <wct9667@rit.edu>
2024-10-08 11:21:44 -0400

    setup random pitch

--- Sample2DProject/Assets/Scripts/Sounds/PlayerFootstepSound.cs
+++ Sample2DProject/Assets/Scripts/Sounds/PlayerFootstepSound.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 [RequireComponent(typeof(AudioSource))]
 public class PlayerFootstepSound : MonoBehaviour
 {
-    [Header("Audio Clips and Volume")]
-    [SerializeField] private AudioClip[] footstepSounds; 
-    [SerializeField] private float footstepVolume = 0.5f; 
+    [Header("Audio Clips and Pitch Range, along with the animator")]
+    [SerializeField] private AudioClip[] footstepSounds;
+    [SerializeField] private Animator animator;
+    [SerializeField] private float footStepVolume = .01f;
+
+    [Range(-3,3)]
+    [SerializeField] private float pitchMin;
+    [Range(-3,3)]
+    [SerializeField] private float pitchMax;
+    
     
 
     private AudioSource audioSource;
@@ -15,21 +22,27 @@ public class PlayerFootstepSound : MonoBehaviour
     void Awake()
     {
         audioSource = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
         audioSource.loop = false;
         audioSource.playOnAwake = false;
     }
     
 
+    /// <summary>
+    /// Plays a footstep if the animator is not in transition.
+    /// </summary>
     public void PlayFootstep()
     {
-        if (footstepSounds.Length > 0)
+        if (/*!animator.IsInTransition(0) &&*/ footstepSounds.Length > 0)
         {
             // Pick a random footstep sound from the array
             int index = Random.Range(0, footstepSounds.Length);
             AudioClip footstepClip = footstepSounds[index];
 
+            //set random pitch
+            audioSource.pitch = Random.Range(pitchMin, pitchMax);
             // Play the sound
-            audioSource.PlayOneShot(footstepClip, footstepVolume);
+            audioSource.PlayOneShot(footstepClip, footStepVolume);
         }
     }
 }
