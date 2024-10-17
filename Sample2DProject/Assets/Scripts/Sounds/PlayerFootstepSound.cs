f93d1e6c7f7cbaccb7c866d0909ef697a92fe8e8
William Tallarico <wct9667@rit.edu>
2024-10-17 09:38:42 -0400

    sounds for footsteps work

--- Sample2DProject/Assets/Scripts/Sounds/PlayerFootstepSound.cs
+++ Sample2DProject/Assets/Scripts/Sounds/PlayerFootstepSound.cs
@@ -8,17 +8,23 @@ public class PlayerFootstepSound : MonoBehaviour
 {
     [Header("Audio Clips and Pitch Range, along with the animator")]
     [SerializeField] private AudioClip[] footstepSounds;
-    [SerializeField] private Animator animator;
     [SerializeField] private float footStepVolume = .01f;
 
     [Range(-3,3)]
     [SerializeField] private float pitchMin;
     [Range(-3,3)]
     [SerializeField] private float pitchMax;
-    
-    
 
-    private AudioSource audioSource;
+    [Header("Animator Thresholds")] 
+    [SerializeField] private float walkThreshold = 0.5f;
+    [SerializeField] private float slowRunThreshold = 1.25f;
+    [SerializeField] private float sprintThreshold = 2.0f;
+   
+    [Header("Additional Needed Components")]
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private Animator animator;
+
+    
 
     void Awake()
     {
@@ -26,28 +32,49 @@ public class PlayerFootstepSound : MonoBehaviour
         animator = GetComponent<Animator>();
         audioSource.loop = false;
         audioSource.playOnAwake = false;
-        SetRandomFootstepSound();
     }
     
 
     /// <summary>
-    /// Plays a footstep if the animator is not in transition.
+    /// Plays a footstep if the weight matches
+    /// Takes in a target speed of the threshold of the animation. This is to prevent double playing.
     /// </summary>
-    public void PlayFootstep()
+    public void PlayFootstep(float speed)
     {
         if (footstepSounds.Length <= 0) return;
-        if (audioSource.isPlaying) return;
-       
-        //set random pitch
-        audioSource.pitch = Random.Range(pitchMin, pitchMax);
-        // Play the sound
-        audioSource.Play();
+
+        //if the movement speed of the animator does not match the speed of the current animation event, do not play
+        //not sure how efficient but better than havuing double playing sounds
+        if(GetMovementStateBasedOnThresholds(animator.GetFloat("speed")) == GetMovementStateBasedOnThresholds(speed))
+        {
+            //set random pitch
+            audioSource.pitch = Random.Range(pitchMin, pitchMax);
+        
+            // Play the sound
+            audioSource.PlayOneShot(GetRandomFootstepSound(), footStepVolume);   
+        }
     }
 
-    private void SetRandomFootstepSound()
+    /// <summary>
+    /// Helper function that grabs a random audio clip.
+    /// </summary>
+    /// <returns></returns>
+    private AudioClip GetRandomFootstepSound()
     {
         int index = Random.Range(0, footstepSounds.Length);
         AudioClip footstepClip = footstepSounds[index];
-        audioSource.clip = footstepClip;
+        return footstepClip;
+    }
+
+    //gets the speed threshold based on a given speed
+    private int GetMovementStateBasedOnThresholds(float speed)
+    {
+        if (speed <= walkThreshold) return 0;
+
+        if (speed <= slowRunThreshold) return 1;
+
+        if (speed <= sprintThreshold) return 2;
+
+        return -1;
     }
 }
