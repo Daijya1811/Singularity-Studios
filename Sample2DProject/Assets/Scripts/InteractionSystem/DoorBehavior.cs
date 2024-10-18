687665930ed5b7552d971c5bfbb41ef61971430b
William Tallarico <wct9667@rit.edu>
2024-10-18 13:34:50 -0400

    doors open and close

---
+++ Sample2DProject/Assets/Scripts/InteractionSystem/DoorBehavior.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorBehavior : MonoBehaviour, IInteractable
+{
+    [SerializeField] private string prompt;
+    
+    [Header("Animator Related Fields")]
+    [SerializeField] private Animator animator;
+    [SerializeField] private string triggerOpenName = "OpenDoor";
+    [SerializeField] private string triggerClosedName = "CloseDoor";
+    private bool isOpen;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        isOpen = false;
+    }
+
+    public string InteractionPrompt => prompt;
+
+    public bool Interact(Interactor interactor)
+    {
+        if (animator.IsInTransition(0)) return true; //return if interacted and in action layer 0
+        if (!isOpen)
+        {
+            animator.SetTrigger(triggerOpenName);
+            isOpen = true;
+        }
+        else
+        {
+            animator.SetTrigger(triggerClosedName);
+            isOpen = false;
+        }
+        
+        Debug.Log("iNTERACTED");
+        return true;
+    }
+}
