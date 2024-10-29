3b00cfc3868afc16b91fc45dc7787c838e50443a
William Tallarico <wct9667@rit.edu>
2024-10-29 11:36:34 -0400

    flashlight UI

--- Sample2DProject/Assets/Scripts/InteractionSystem/IInteractable.cs
+++ Sample2DProject/Assets/Scripts/InteractionSystem/IInteractable.cs
@@ -1,4 +1,4 @@
-// Define the InteractionAllowed enum
+// InteractionAllowEnum to allow for certain types of interactions
 public enum InteractionAllowed
 {
     None,         
@@ -11,7 +11,10 @@ public interface IInteractable
 {
     public string InteractionPrompt { get; }
     
+    //used to determine who can interact with an object
     public InteractionAllowed WhoCanInteract { get; }
+    
+    //flag for UI prompt change
     bool PromptUpdated { get; set; } 
 
     public bool Interact(Interactor interactor);
