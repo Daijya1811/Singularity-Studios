31f861903d96f0e410b50e1be03bd24806d854fb
Jonathan Benz <98258254+JonathanBenz@users.noreply.github.com>
2024-11-16 20:06:19 -0500

    Bug Fixed Dynamic Camera jitters

--- Sample2DProject/Assets/Scripts/Camera/CamerasController.cs
+++ Sample2DProject/Assets/Scripts/Camera/CamerasController.cs
@@ -11,11 +11,22 @@ public class CamerasController : MonoBehaviour
 {
     List<Camera> playerCameras = new List<Camera>();
     Camera mainCamera;
+    float camHeight;
+    float camWidth;
+
+    Vector3 p1Pos;
+    Vector3 p2Pos;
+    bool isMain = true;
+    bool isSplit = false;
+    FollowCamera followCamera;
+    float midPointX;
+    float midPointZ;
 
     [SerializeField] private Camera uiCamera;
     void Awake()
     {
         mainCamera = Camera.main;
+        followCamera = GetComponent<FollowCamera>();
     }
 
     private void Start()
@@ -26,6 +37,29 @@ public class CamerasController : MonoBehaviour
         playerCameras.Add(playerCams[1].GetComponent<Camera>());
         playerCameras.Add(playerCams[2].GetComponent<Camera>());
         playerCameras.Add(playerCams[3].GetComponent<Camera>());
+        camHeight = 2f * mainCamera.orthographicSize;
+        camWidth = camHeight * mainCamera.aspect;
+    }
+    private void Update()
+    {
+        // If there is only one player in the scene, then don't do anything. Return. 
+        if (followCamera.Players[1] == null) return;
+
+        p1Pos = followCamera.Players[0].transform.position;
+        p2Pos = followCamera.Players[1].transform.position;
+        midPointX = (p2Pos.x + p1Pos.x) / 2f;
+        midPointZ = (p2Pos.z + p1Pos.z) / 2f;
+
+        // if cam is not main yet both players are in bound, then transition to main cam
+        if (!isMain && CheckForBounds(p1Pos) && CheckForBounds(p2Pos))
+        {
+            ToggleCamera();
+        }
+        // if cam is not split yet either player is out of bound, then transition to split cam
+        else if((!isSplit && !CheckForBounds(p1Pos)) || (!isSplit && !CheckForBounds(p2Pos)))
+        {
+            ToggleCamera();
+        }
     }
 
     /// <summary>
@@ -35,9 +69,49 @@ public class CamerasController : MonoBehaviour
     {
         mainCamera.enabled = !mainCamera.enabled;
         uiCamera.enabled = !uiCamera.enabled;
+        isMain = !isMain;
+        isSplit = !isSplit;
         foreach(Camera camera in playerCameras)
         {
             camera.enabled = !camera.enabled;
+        }
+    }
+
+    private bool CheckForBounds(Vector3 PlayerPos)
+    {
+        if (PlayerPos.x > midPointX + camWidth / 2f)
+        {
+            return false;
+        }
+        if (PlayerPos.x < midPointX - camWidth / 2f)
+        {
+            return false;
+        }
+        if (PlayerPos.z > midPointZ + camHeight / 2f)
+        {
+            return false;
+        }
+        if (PlayerPos.z < midPointZ - camHeight / 2f)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(Application.isPlaying)
+        {
+            Gizmos.color = Color.yellow;
+            Vector3 topLeftCorner = new Vector3(midPointX - camWidth / 2f, 0f, midPointZ + camHeight / 2f);
+            Vector3 topRightCorner = new Vector3(midPointX + camWidth / 2f, 0f, midPointZ + camHeight / 2f);
+            Vector3 bottomLeftCorner = new Vector3(midPointX - camWidth / 2f, 0f, midPointZ - camHeight / 2f);
+            Vector3 bottomRightCorner = new Vector3(midPointX + camWidth / 2f, 0f, midPointZ - camHeight / 2f);
+            Gizmos.DrawLine(topLeftCorner, topRightCorner);
+            Gizmos.DrawLine(topRightCorner, bottomRightCorner);
+            Gizmos.DrawLine(bottomRightCorner, bottomLeftCorner);
+            Gizmos.DrawLine(bottomLeftCorner, topLeftCorner);
+            Gizmos.DrawSphere(new Vector3(midPointX, 0f, midPointZ), 1f);
         }
     }
 }
