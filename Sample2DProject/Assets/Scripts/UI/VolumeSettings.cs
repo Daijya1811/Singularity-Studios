5faaee4043789738faa20ed06faefd78e9ddb9df
William Tallarico <wct9667@rit.edu>
2024-11-19 13:00:49 -0500

    main menu UI optimization

---
+++ Sample2DProject/Assets/Scripts/UI/VolumeSettings.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeSettings : MonoBehaviour
+{
+    [SerializeField] private AudioMixer mixer;
+    [SerializeField] private Slider masterSlider;
+
+    private const string MIXER_MASTER = "Master";
+    void Awake()
+    {
+        masterSlider.onValueChanged.AddListener(SetMasterVolume);
+    }
+
+    void SetMasterVolume(float value)
+    {
+        mixer.SetFloat(MIXER_MASTER, Mathf.Log10(value) * 20); 
+    }
+}
