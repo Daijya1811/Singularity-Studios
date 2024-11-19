7b6c4ba51e83fd3a870973bfa5d894a06e69497e
William Tallarico <wct9667@rit.edu>
2024-11-19 12:18:02 -0500

    removed uneeded components and modified script

--- Sample2DProject/Assets/Audio/AudioManager.cs
+++ Sample2DProject/Assets/Audio/AudioManager.cs
@@ -1,11 +1,8 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class AudioManager : MonoBehaviour
 {
-    [SerializeField] AudioClip[] sfx;
 
     AudioSource audioSource;
 
