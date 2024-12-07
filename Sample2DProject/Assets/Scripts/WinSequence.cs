1391d3a06bff3798aa0410d748a43bd33226a770
Jonathan Benz <98258254+JonathanBenz@users.noreply.github.com>
2024-12-07 01:20:47 -0500

    End Sequence!

--- Sample2DProject/Assets/Scripts/WinSequence.cs
+++ Sample2DProject/Assets/Scripts/WinSequence.cs
@@ -2,11 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
 
 public class WinSequence : MonoBehaviour
 {
+    [SerializeField] Fader winFader;
+
+    private void OnEnable()
+    {
+        GetComponent<PlayableDirector>().stopped += ReturnToMainMenu;
+    }
+    private void OnDisable()
+    {
+        GetComponent<PlayableDirector>().stopped -= ReturnToMainMenu;
+    }
     private void OnTriggerEnter(Collider other)
     {
         GetComponent<PlayableDirector>().Play();
+        GetComponentInChildren<Animator>().enabled = true;
+    }
+
+    void ReturnToMainMenu(PlayableDirector pd)
+    {
+        SceneManager.LoadScene(0);
     }
 }
